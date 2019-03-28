0ccb766379beccdeeea3263371a8c3408a72c60d
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-03-28 15:10:12 +1100

    Delete ch07_hw1.py

--- Chapter Seven: Files/ch07_hw1.py
+++
@@ -1,6 +0,0 @@
-print('Exercise 1: Write a program to read through a file and '
-      'print the contents of the file (line by line) all in upper case.')
-fname = open ('mbox.txt')
-for line in fname:
-    line = line.rstrip()
-    print(line.upper())
